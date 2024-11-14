bfd0c8a9932962ebeca2b6509223884acbf12afe
USERNAME <EMAIL>
2024-11-14 19:07:05 +0000

    updated test file

--- Dex2HexTest.java
+++ Dex2HexTest.java
@@ -1,1 +1,36 @@
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
+public class Dex2HexTest {
+
+    public Dex2HexTest()
+   {}
+    Dec2Hex dec2Hex;
+
+    @Before
+    public void setUp() {
+        dec2Hex = new Dec2Hex();
+    }
+
+	@Test
+    void testValidIntegerInput() {
+
+        String[] args = {"15"};
+        Dec2Hex.main(args);
+	}
+
+	@Test
+    void testNonIntegerInput() {
+
+        String[] args = {"1.5"};
+        Dec2Hex.main(args);
+	}
+
+	@Test
+    void testNoIntegerInput() {
+
+        String[] args = {""};
+        Dec2Hex.main(args);
+	}
+}
