1ceed802b8834213194444e0c6eecfb0d3fa2bef
USERNAME <EMAIL>
2024-11-14 15:33:40 +0000

    added in input checks for Dex2Hex

--- Dex2Hex.java
+++ Dex2Hex.java
@@ -4,7 +4,20 @@ class Dec2Hex
 {
 public static int Arg1;
     public static void main(String args[])    {
-        Arg1 = Integer.parseInt(args[0]);
+
+	if (args.length == 0) {
+            System.out.println("Error: No input argument was provided");
+            return;
+        }
+
+        try {
+            Arg1 = Integer.parseInt(args[0]);
+        } catch (NumberFormatException e) {
+            System.out.println("Error: Non-integer input provided.");
+            return;
+        }
+
+
         char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
         int rem, num;
         num = Arg1;
@@ -21,4 +34,3 @@ public static int Arg1;
         System.out.println("Hexadecimal representation is: " + hexadecimal);
 
     }
-}
