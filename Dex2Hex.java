2b3a22fd193fcfe627aec7fd99cb8d57fc3fcf46
Ubuntu <ubuntu@ip-172-31-23-81.ec2.internal>
2024-11-15 16:23:00 +0000

    final code for Dex2Hex.java

--- Dex2Hex.java
+++ Dex2Hex.java
@@ -1,25 +1,33 @@
 import java.util.Scanner;
 
-class Dec2Hex
-{
-public static int Arg1;
-    public static void main(String args[])    {
-        Arg1 = Integer.parseInt(args[0]);
-        char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
-        int rem, num;
-        num = Arg1;
-        String hexadecimal="";
-        System.out.println("Converting the Decimal Value " + num + " to Hex...");
-
-        while(num != 0)
-        {
-            rem=num%16;
-            hexadecimal= ch[rem] + hexadecimal;
-            num= num/16;
+public class Dex2Hex {
+    public static String dexToHexConversion(String[] args) {
+        if (args.length == 0) {
+            return "Error: No input argument provided. Ending Program.";
         }
 
-        System.out.println("Hexadecimal representation is: " + hexadecimal);
+        int num;
+        try {
+            num = Integer.parseInt(args[0]);
+        } catch (NumberFormatException e) {
+            return "Error: Non-integer input provided. Ending Program.";
+        }
 
+        char[] ch = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+        String hexadecimal = "";
+        int rem;
+
+        while (num != 0) {
+            rem = num % 16;
+            hexadecimal = ch[rem] + hexadecimal;
+            num = num / 16;
+        }
+
+        return "Converting the Decimal Value " + args[0] + " to Hex...\n" +
+               "Hexadecimal representation is: " + hexadecimal;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(dexToHexConversion(args));
     }
 }
-
